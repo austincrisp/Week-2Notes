3e4e72679e0d4159dbdf5702bdbb1a852d72a995
austincrisp <austi@AUSTINBLADE>
2017-01-17 10:22:43 -0500

    Started OOP

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace Day2
 {
+    // Class Definition
+    class Dog
+    {
+        private int Age;
+        public string Name;
+
+        public Dog(string _Name)
+        {
+            Age = 0;
+            Name = _Name;
+        }
+
+        public void HappyBirthday()
+        {
+            Age++;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public int GetAgeDogYears()
+        {
+            return Age * 7;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var wilkins = new Dog("Wilkins");
+            wilkins.HappyBirthday();
+            var age = wilkins.GetAge();
+            var ageDogYears = wilkins.GetAgeDogYears();
+            Console.WriteLine(age);
         }
     }
 }
